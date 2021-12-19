03cb80bbe4f3326cf3123fcdfc67be9dd17b129e
wohpe <83101710+wohpe@users.noreply.github.com>
2021-12-19 10:34:06 +0900

    Update Uhh.lua

--- Uhh.lua
+++ Uhh.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wohpe/KR_HUB_MainScript/main/Key.lua?token=AT2AQDSU7DZ32Z3PUSDYH3DBX2E3A"),true)()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wohpe/KR_HUB_MainScript/main/Key.lua?token=AT2AQDSBARSBF6TTLXDSERDBX2GC2"),true)()
